4917095b1abe91a063208b19c181c2bdb952909e
yuryqwer <yuryqwer@outlook.com>
2022-01-23 22:34:43 +0800

    设置元类，通过类变量和元类的__call__方法实现单例模式

--- 单例模式/singleton2.py
+++ 单例模式/singleton2.py
@@ -6,6 +6,11 @@ class MetaSingleton(type):
 
 
 class Logger(metaclass=MetaSingleton):
+    """
+    利用类变量在不同实例中的唯一性，将类变量作为全局标志位
+    在调用元类的__call__方法创建实例时，访问此标志位
+    如果不存在则创建实例，否则直接返回最初创建的实例
+    """
     def __init__(self, file_name):
         if not hasattr(self, "file_name"):
             self.file_name = file_name
